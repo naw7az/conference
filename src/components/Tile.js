54bd1bced309f5f18958b5fcd7fb57b32fa6fdba
naw7az <nawazadil17@outlook.com>
2022-07-02 01:36:49 +0530

    testing pre commit

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,35 +1,47 @@
-import React from 'react';
-import './Tile.css'
+import React from "react";
+import "./Tile.css";
 
 const Tile = ({ image, startDate, endDate, name, place, link, entryType }) => {
-
   const imageExists = (image_src) => {
+    let s;
+
+    console.log('eeeeee')
 
     while (image_src.includes('"')) {
-      image_src = image_src.replace('"', '')
+
+      
+      image_src = image_src.replace('"', "");
     }
-    if (image_src.slice(0, 1) === 'h'){
+    if (image_src.slice(0, 1) === "h") {
       return image_src;
+    } else {
+      return "https://image.freepik.com/free-photo/brick-wall-painted-white_1194-7526.jpg";
     }
-    else {
-      return 'https://image.freepik.com/free-photo/brick-wall-painted-white_1194-7526.jpg'
-    }
-  }
+  };
 
   return (
-    <div className='tc grow bg-white br3 pa3 ma2 dib bw2 shadow-5 card'>
-      <img alt='conference' src={imageExists(image)} width='100%' height='200px'/>
+    <div className="tc grow bg-white br3 pa3 ma2 dib bw2 shadow-5 card">
+      <img
+        alt="conference"
+        src={imageExists(image)}
+        width="100%"
+        height="200px"
+      />
       <div>
         <h3> {name} </h3>
         <p> Starting Date: {startDate} </p>
         <p> Ending Date: {endDate} </p>
-        <p> Place: {place? place: 'N/A'} </p>
-        <p> Entry: <strong>{entryType}</strong> </p>
-        <a href={link} target='_blank' rel="noopener noreferrer">Apply Here</a>
-        
+        <p> Place: {place ? place : "N/A"} </p>
+        <p>
+          {" "}
+          Entry: <strong>{entryType}</strong>{" "}
+        </p>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          Apply Here
+        </a>
       </div>
     </div>
   );
-}
+};
 
 export default Tile;
